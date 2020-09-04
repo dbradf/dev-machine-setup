19f4eb6937874ed1ba8db3a397120edb8277f431
David Bradford <david.bradford@mongodb.com>
2020-09-04 07:21:17 -0400

    fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 python -m venv --system-site-packages --python=python3 venv
-source venv/bin/activate
+source ./venv/bin/activate
 pip install ansible
 
 ansible-playbook dev-setup.yml
